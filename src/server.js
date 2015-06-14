932b99e6d3d6d2ca2febd1af9405cea4e6a665dc
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-06-14 22:56:37 +1000

    when there are no modes; don't use multi-mode routes

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ module.exports = {
     };
 
     var configureRoutes = function (app, modes) {
-      if (modes.length > 0) {
+      if (modes.length > 0 && (modes[0] !== 'game')) {
         configureMultiModeGame(app);
       } else {
         configureSingleModeGame(app);
