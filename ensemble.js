16ce80a493dc9f4e76db46ea7664a0f85429bfd6
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-06-26 20:49:58 +1000

    remove cli aspect of this project

--- ensemble.js
+++ ensemble.js
@@ -1,9 +1,3 @@
-#!/usr/bin/env node
 'use strict';
 
-var path = process.argv[2];
-if (path === undefined) {
-  throw 'No game path specified';
-}
-
-require('./src/facade').runGameAtPath(path);
+module.exports = require('./src/facade');
