1d83324f339eb4d03ab86a92388f3f1943a4b62c
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-08-20 17:47:13 +1000

    Add mode support for views

--- src/events/client/initialise-views.js
+++ src/events/client/initialise-views.js
@@ -1,16 +1,25 @@
 'use strict';
 
 var each = require('lodash').each;
+var first = require('lodash').first;
+var filter = require('lodash').filter;
+var intersection = require('lodash').intersection;
+var last = require('lodash').last;
 
 module.exports = {
   type: 'OnSetup',
-  deps: ['View', 'Dimensions'],
-  func: function OnSetup (views, dimensions) {
+  deps: ['View', 'Dimensions', 'GameMode'],
+  func: function OnSetup (views, dimensions, mode) {
     return function initialiseViews () {
       var dims = dimensions().get();
 
-      each(views(), function(view) {
-        view(dims);
+      function hasMatchingMode(callback) {
+        return intersection(['*', mode()], first(callback)).length > 0;
+      }
+
+      var applicableCallbacks = filter(views(), hasMatchingMode);
+      each(applicableCallbacks, function(view) {
+        last(view)(dims);
       });
     };
   }
