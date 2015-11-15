b317da63adf6c632c6992ea54f78cacd70f39627
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-11-15 17:07:48 +1100

    better log support when testing

--- tests/fake/logger.js
+++ tests/fake/logger.js
@@ -2,17 +2,32 @@
 
 var sinon = require('sinon');
 
+function logMessageOnly () {
+  if (arguments.length === 1) {
+    console.log(arguments[0]);
+  } else {
+    console.log(arguments[1]);
+  }
+}
+
 var logging = {
   loaded: sinon.spy(),
   plugin: sinon.spy(),
   subdue: sinon.spy(),
   called: sinon.spy(),
-  debug: sinon.spy(),
+  debug: logMessageOnly,
   filename: sinon.spy(),
-  info: sinon.spy(),
-  warn: sinon.spy(),
-  error: sinon.spy(),
-  socket: sinon.spy()
+  info: logMessageOnly,
+  warn: logMessageOnly,
+  error: logMessageOnly,
+  socket: sinon.spy(),
+  trace: logMessageOnly
 };
+
+sinon.spy(logging, 'trace');
+sinon.spy(logging, 'debug');
+sinon.spy(logging, 'info');
+sinon.spy(logging, 'warn');
+sinon.spy(logging, 'error');
 
 module.exports = logging;
