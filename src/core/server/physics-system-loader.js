6b82bd29b367e7d83ec72a657c316cf72ca4c9fe
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-12-03 08:40:29 +1100

    name yer functions

--- src/core/server/physics-system-loader.js
+++ src/core/server/physics-system-loader.js
@@ -5,7 +5,7 @@ var each = require('lodash').each;
 module.exports = {
   type: 'OnGameReady',
   deps: ['PhysicsMap', 'StateTracker', 'PhysicsSystem', 'StateAccess'],
-  func: function (physicsMap, tracker, physicsSystem, state) {
+  func: function OnGameReady (physicsMap, tracker, physicsSystem, state) {
     return function wireupPhysicsMap (game) {
       each(physicsMap(), function(sources, key) {
         each(sources, function(source) {
