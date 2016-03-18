5b8dee3b5b81d9740bfcefd55acc43247257035d
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-03-19 10:22:31 +1100

    player state initialisation

--- src/state/server/initialise-state-for-player.js
+++ src/state/server/initialise-state-for-player.js
@@ -1,16 +1,18 @@
 'use strict';
 
-import {each, last} from 'lodash';
+import {each, last, merge} from 'lodash';
 var filterPluginsByMode = require('../../util/modes').filterPluginsByMode;
 
 
 function OnNewPlayer (playerStateSeeds, mutate) {
   return function initialiseStateForSave (save, playerId) {
-    mutate()(save.id, ['players+', {id: playerId}]);
+    var playerState = {id: playerId};
 
     each(filterPluginsByMode(playerStateSeeds(), save.mode), seed => {
-      mutate()(save.id, [`players:${playerId}`, last(seed)(playerId)]);
+      playerState = merge(playerState, last(seed)(playerId));
     });
+
+    mutate()(save.id, ['players+', playerState]);
   };
 }
 
