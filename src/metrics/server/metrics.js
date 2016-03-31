26f03bb55cbe6c2a768298da1f02c46ef11b62ed
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-03-31 17:17:36 +1100

    new clipboard version, new gulp version, removed unused code from metrics

--- src/metrics/server/metrics.js
+++ src/metrics/server/metrics.js
@@ -13,7 +13,6 @@ var moment = require('moment');
 
 var endpoint = config.get().measure.endpoint;
 var appId = config.get().game.id;
-const sessionStartTime = moment().unix();
 
 function post (event, url) {
   if (!config.get().server.metrics) {
