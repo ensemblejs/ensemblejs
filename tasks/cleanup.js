41df28592536eab02d959cbd10456d0cf492083f
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-04-04 23:07:54 +1000

    updated most of the deps

--- tasks/cleanup.js
+++ tasks/cleanup.js
@@ -5,14 +5,14 @@ var paths = require('./paths');
 
 function addTasks (gulp) {
   gulp.task('delete-targets', function (done) {
-      del(paths.targets.clean, done);
+      del(paths.targets.clean).then(() => done());
   });
 
   gulp.task('project:clean', ['delete-targets', 'db:reset']);
 
 
-  gulp.task('framework:delete-paths', function (cb) {
-      del(paths.framework.clean, cb);
+  gulp.task('framework:delete-paths', function (done) {
+      del(paths.framework.clean).then(() => done());
   });
 
   gulp.task('framework:clean', ['framework:delete-paths']);
