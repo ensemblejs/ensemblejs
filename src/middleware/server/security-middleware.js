410e57f5d310813edc1d3eec3f209f3eb321856c
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-04-06 21:31:59 +1000

    make the CSP allowed sources a touch more readable

--- src/middleware/server/security-middleware.js
+++ src/middleware/server/security-middleware.js
@@ -34,11 +34,21 @@ module.exports = {
       });
     }
 
+    var productionAllowedSources = [
+      "'self'",
+      'ws://' + process.env.HOSTNAME
+    ];
+    var nonProductionAllowedSources = [
+      "'self'",
+      'ws://localhost:3000',
+      'http://localhost:4000'
+    ]
+
     define()('WebServerMiddleware', function SecurityMiddleware () {
       //jshint quotmark:false
       // var reportOnly = process.env.NODE_ENV !== 'production';
       var reportOnly = true;
-      var connectSrc = process.env.NODE_ENV === 'production' ? ["'self'", 'ws://' + process.env.HOSTNAME] : ["'self'", 'ws://localhost:3000', 'http://localhost:4000'];
+      var connectSrc = process.env.NODE_ENV === 'production' ? productionAllowedSources : nonProductionAllowedSources;
 
       return csp({
         directives: {
