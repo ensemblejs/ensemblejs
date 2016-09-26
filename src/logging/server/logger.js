47d9fbabf07387b937df211c033e612c62c1eac6
andy shaw <shawry@shawry.com>
2016-09-26 11:05:30 +1000

    Use console as a temporary replacement for bunyan.

--- src/logging/server/logger.js
+++ src/logging/server/logger.js
@@ -3,11 +3,9 @@
 const appRoot = require('app-root-path');
 const packageInfo = require(`${appRoot}/package.json`);
 
-export const logger = require('bunyan').createLogger({
-  name: packageInfo.name,
-  version: packageInfo.version,
-  streams: [{ level: 'debug', stream: process.stdout }]
-});
+console.debug = function(msg) { console.log(msg) }
+
+export const logger = console
 
 export const setLogLevel = (config) => {
   logger.logLevel = config.logging.logLevel;
